5768d866f2703a7b1cb2cf9c03b20c1d8207e969
aka-v <juandimorealvi05@gmail.com>
2025-07-12 18:44:45 -0400

    add the files were separated into folders for more order according to the module

--- progra-Platzi/1 module programming fundamentals/chains.py
+++ progra-Platzi/1 module programming fundamentals/chains.py
@@ -3,8 +3,10 @@ name = "Mateo"
 
 fullhi =  hi + ' ' + name
 
+print(5 * name)
 print(len(fullhi))
 print(fullhi.upper())
 print(name.lower())
+print(name.upper())
 
 # No me acuerdo que era lo de esta clase
