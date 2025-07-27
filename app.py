dfe13f5597538bfc748d4e7ab86181ad696950eb
aka-v <juandimorealvi05@gmail.com>
2025-07-27 19:28:58 -0400

    Add more order on module 7 and add analitics_dates_sales.py

--- app.py
+++ app.py
@@ -1,20 +1,2 @@
-# Clase 34 P3 RANDOM
 
-import random
-
-# Generar un numero entero aleatorio
-random_number = random.randint(1, 10) 
-
-print(random_number)
-
-# Elegir colores aleatorios
-colors = ['Rojo', 'Azul', 'Amarillo']
-random_colors = random.choice(colors)
-
-print(random_colors)
-
-# Barajar una lista de cartas
-cards = ['As', 'Rey', 'Reina', 'Jota', '10']
-random.shuffle(cards)
-
-print(cards)
+print("Hola Mundo")
